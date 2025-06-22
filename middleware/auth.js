edce6a9f2399a3284318c0e763bfc5eb973fed7c
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-22 10:57:57 +0100

    updated libraries/ packages and authMiddleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,24 @@
-const User = require('../models/User') // Make sure you have this
+const User = require('../models/User')
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const { user } = req.body; // or req.session.user / req.user
+    const { user } = req.body;
 
     if (!user) {
       return res.status(401).json({ message: 'No user provided' });
     }
 
-    const isUser = await User.findOne({ username: user }); // or email, or _id
+    const isUser = await User.findOne({ username: user }); 
 
     if (!isUser) {
       return res.status(401).json({ message: 'User does not exist' });
     }
 
-    req.user = isUser; // Attach user to request for downstream use
-    next(); // Pass control to next middleware/route
+    req.user = isUser; 
+    next(); 
   } catch (err) {
     res.status(500).json({ message: 'Auth middleware failed', error: err.message });
   }
 };
+
+module.exports = authMiddleware
